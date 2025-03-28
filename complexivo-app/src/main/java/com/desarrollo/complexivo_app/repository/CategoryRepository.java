a41e8cc67271041222774391c0c99fba0a4bff46
Karma2502 <133880048+Karma2502@users.noreply.github.com>
2025-03-28 11:25:51 -0500

    Delete complexivo-app directory

--- complexivo-app/src/main/java/com/desarrollo/complexivo_app/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.desarrollo.complexivo_app.repository;
-
-import com.desarrollo.complexivo_app.models.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository <Category, Long> {
-}
--- complexivo-app/src/main/java/com/desarrollo/complexivo_app/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.desarrollo.complexivo_app.repository;
-
-import com.desarrollo.complexivo_app.models.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
